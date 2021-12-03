4e06e07839bdd5ced560f3a5a218adbdf278e9cf
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-12-02 22:35:51 -0500

    tabs

--- regex/regexLib/src/Compiler.cpp
+++ regex/regexLib/src/Compiler.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 
 State* Compiler::compile(const std::string& pattern) {
-	if(pattern == "") return nullptr;
+  if (pattern == "") return nullptr;
 
   const std::string postfixRegex = infixToPostfix(pattern);
   for (char c : postfixRegex) {
@@ -11,18 +11,18 @@ State* Compiler::compile(const std::string& pattern) {
       case '.':
         concatinateStates();
         break;
-			case '|':
-				createOrState();
-				break;
-			case '?':
-				zeroOrOneState();
-				break;
+      case '|':
+        createOrState();
+        break;
+      case '?':
+        zeroOrOneState();
+        break;
       default:
         createNormalState(c);
         break;
     }
   }
-	createFinalMatchState();
+  createFinalMatchState();
   return NFAfrags.top().start;
 }
 
@@ -34,21 +34,21 @@ void Compiler::patch(std::vector<State**>& outList, State* const state) {
 
 void Compiler::createNormalState(const char c) {
   State* s = new State(State::DEFAULT, c, nullptr, nullptr);
-	Frag frag = {s, {&s->out1}};
+  Frag frag = {s, {&s->out1}};
   NFAfrags.push(frag);
 }
 
-void Compiler::createOrState()
-{
-	Frag f2 = NFAfrags.top();
+void Compiler::createOrState() {
+  Frag f2 = NFAfrags.top();
   NFAfrags.pop();
   Frag f1 = NFAfrags.top();
   NFAfrags.pop();
 
-	State* orState = new State(State::SWITCH, '\0', f1.start, f2.start);
-	std::copy(f1.outList.begin(), f1.outList.end(), std::back_inserter(f2.outList));
-	Frag newFrag = {orState, f2.outList};
-	NFAfrags.push(newFrag);
+  State* orState = new State(State::SWITCH, '\0', f1.start, f2.start);
+  std::copy(f1.outList.begin(), f1.outList.end(),
+            std::back_inserter(f2.outList));
+  Frag newFrag = {orState, f2.outList};
+  NFAfrags.push(newFrag);
 }
 
 void Compiler::concatinateStates() {
@@ -57,27 +57,25 @@ void Compiler::concatinateStates() {
   Frag f1 = NFAfrags.top();
   NFAfrags.pop();
 
-	patch(f1.outList, f2.start);
-	
-	Frag newFrag(f1.start, f2.outList);
-	NFAfrags.push(newFrag);
+  patch(f1.outList, f2.start);
+
+  Frag newFrag(f1.start, f2.outList);
+  NFAfrags.push(newFrag);
 }
 
-void Compiler::zeroOrOneState()
-{
-	Frag frag = NFAfrags.top();
-	NFAfrags.pop();
+void Compiler::zeroOrOneState() {
+  Frag frag = NFAfrags.top();
+  NFAfrags.pop();
 
-	State* state = new State(State::SWITCH, '\0', frag.start, nullptr);
-	frag.outList.push_back(&state->out2);
-	Frag newFrag(state, frag.outList);
+  State* state = new State(State::SWITCH, '\0', frag.start, nullptr);
+  frag.outList.push_back(&state->out2);
+  Frag newFrag(state, frag.outList);
 
-	NFAfrags.push(newFrag);
+  NFAfrags.push(newFrag);
 }
 
-void Compiler::createFinalMatchState()
-{
-	Frag& frag = NFAfrags.top();
-	State* finalState = new State(State::MATCH, '\0', nullptr, nullptr);
-	patch(frag.outList, finalState);
+void Compiler::createFinalMatchState() {
+  Frag& frag = NFAfrags.top();
+  State* finalState = new State(State::MATCH, '\0', nullptr, nullptr);
+  patch(frag.outList, finalState);
 }
